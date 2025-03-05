57c777afcf9892ec3351c583ed0d0c366a0ed8fd
Myrkri <pertsevoi@yahoo.com>
2025-03-05 04:01:43 +0200

    removed extra kafka broker host

--- src/main/java/com/pet/project/kafkamessenger/config/KafkaConfig.java
+++ src/main/java/com/pet/project/kafkamessenger/config/KafkaConfig.java
@@ -17,7 +17,7 @@ public class KafkaConfig {
     @Bean
     public KafkaConsumer<String, MessageMetadataDTO> consumer() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9094");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "chat-messenger");
